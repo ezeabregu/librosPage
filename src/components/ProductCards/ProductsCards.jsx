87f314b944520d78f3166c5fe35e23dc92716e85
ezeabregu <ezeabregu.ea@gmail.com>
2024-05-09 23:20:23 -0300

    Filtro ok

--- src/components/ProductCards/ProductsCards.jsx
+++ src/components/ProductCards/ProductsCards.jsx
@@ -7,39 +7,15 @@ import {
 } from "./productCardsStyles";
 import { Libros } from "../../data/libros";
 import Product from "./Product";
-import Button from "../Button/Button";
 
 const ProductsCards = () => {
-  let LibrosCopy = Libros;
+  let LibrosCopy = {};
 
-  const [value, setValue] = useState("");
-  const listOfBooks = Libros.map((libro) => {
-    return libro.title;
-  });
-
-  const handlerSubmitSearch = (e) => {
-    e.preventDefault();
-
-    const newSearch = value.trim();
-
-    const libroSearch = listOfBooks.find((libro) => {
-      return libro.toLowerCase() === newSearch;
-    });
-
-    if (libroSearch) {
-      LibrosCopy = Libros.filter((libro) => {
-        return libro.title.toLowerCase().includes(libroSearch.toLowerCase());
-      });
-
-      doScroll();
-      // const obtenerLibro = LibrosCopy.find((libro) => {
-      //   return libro.title === libroSearch;
-      // });
-    } else {
-      return alert("Libro no encontrado, prueba con otro!");
-    }
-    setValue("");
-  };
+  // const [value, setValue] = useState("");
+  const [search, setSearch] = useState("");
+  // const listOfBooks = Libros.map((libro) => {
+  //   return libro.title;
+  // });
 
   const librosRef = useRef();
 
@@ -50,6 +26,43 @@ const ProductsCards = () => {
     );
   };
 
+  const searcher = (e) => {
+    setSearch(e.target.value);
+  };
+
+  if (!search) {
+    LibrosCopy = Libros;
+  } else {
+    LibrosCopy = Libros.filter((dato) => {
+      return dato.title.toLowerCase().includes(search.toLowerCase());
+    });
+    doScroll();
+  }
+
+  // const handlerSubmitSearch = (e) => {
+  //   e.preventDefault();
+
+  //   const newSearch = value.trim();
+
+  //   const libroSearch = listOfBooks.find((libro) => {
+  //     return libro.toLowerCase() === newSearch;
+  //   });
+
+  //   if (libroSearch) {
+  //     LibrosCopy = Libros.filter((libro) => {
+  //       return libro.title.toLowerCase().includes(libroSearch.toLowerCase());
+  //     });
+
+  //     doScroll();
+  //     // const obtenerLibro = LibrosCopy.find((libro) => {
+  //     //   return libro.title === libroSearch;
+  //     // });
+  //   } else {
+  //     return alert("Libro no encontrado, prueba con otro!");
+  //   }
+  //   setValue("");
+  // };
+
   return (
     <>
       <ProductCardsStyledContainer doScroll={doScroll}>
@@ -57,12 +70,14 @@ const ProductsCards = () => {
         <FormStyled>
           <InputFormStyled
             placeholder="Buscar un libro..."
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            // value={value}
+            // onChange={(e) => setValue(e.target.value)}
+            onChange={searcher}
+            value={search}
           />
-          <Button onClick={(e) => handlerSubmitSearch(e)} disabled={!value}>
+          {/* <Button onClick={(e) => handlerSubmitSearch(e)} disabled={!value}>
             Buscar
-          </Button>
+          </Button> */}
         </FormStyled>
         <ProductsContainer ref={librosRef}>
           {LibrosCopy.map((libro) => {
