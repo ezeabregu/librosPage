2a601279f88db6f494ee0ea37180326085a44f69
ezeabregu <ezeabregu.ea@gmail.com>
2024-06-27 21:30:42 -0300

    prueba cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const INITIAL_LIMIT = 5;
 export const SHIPPING_COST = 1500;
 export const INITIAL_LIMIT_BOOKS = 20;
-export const BASE_URL = "https://page-api.vercel.app";
+export const BASE_URL = "https://page-api.vercel.app/";
 //export const BASE_URL = "http://localhost:8080";
