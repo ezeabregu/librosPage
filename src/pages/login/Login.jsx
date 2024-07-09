f47cb3156b0a22dad9464200db4282e7b293ce6d
ezeabregu <ezeabregu.ea@gmail.com>
2024-07-08 22:29:08 -0300

    cambios user

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,55 +9,60 @@ import { ErrorStyled } from "../../components/Formulario/formularioStyles";
 import ButtonForm from "../../components/ButtonForm/ButtonForm";
 import { LinkButton } from "../../components/Hero/heroStyles";
 import { loginUser } from "../../axios/axiosUser";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setCurrentUser } from "../../redux/user/userSlice";
 
 const Login = () => {
+  const currentUser = useSelector((state) => state.user.currentUser);
   const dispatch = useDispatch();
   //useRedirect("/");
 
   return (
     <>
-      <Formik
-        initialValues={loginValues}
-        validationSchema={validationLogin}
-        onSubmit={async (values, actions) => {
-          const user = await loginUser(values.email, values.password);
-          if (user) {
-            dispatch(
-              setCurrentUser({
-                ...user.usuario,
-                token: user.token,
-              })
-            );
-          }
-          actions.resetForm();
-        }}
-      >
-        <LoginContainer>
-          <h2>PAGE</h2>
-          <label>¡El libro que buscás, esta acá!</label>
-          <Field
-            name="email"
-            type="email"
-            id="email"
-            as={InputStyled}
-            placeholder="Correo electrónico"
-          ></Field>
-          <ErrorMessage name="email" component={ErrorStyled} />
-          <Field
-            name="password"
-            type="password"
-            id="password"
-            as={InputStyled}
-            placeholder="Contraseña"
-          ></Field>
-          <ErrorMessage name="password" component={ErrorStyled} />
-          <ButtonForm>Inicia sesión</ButtonForm>
-          <h3>¿No tienes una cuenta?</h3>
-          <LinkButton to="/register">Regístrate</LinkButton>
-        </LoginContainer>
-      </Formik>
+      {currentUser ? (
+        <h1>Cerrar sesion</h1>
+      ) : (
+        <Formik
+          initialValues={loginValues}
+          validationSchema={validationLogin}
+          onSubmit={async (values, actions) => {
+            const user = await loginUser(values.email, values.password);
+            if (user) {
+              dispatch(
+                setCurrentUser({
+                  ...user.usuario,
+                  token: user.token,
+                })
+              );
+            }
+            actions.resetForm();
+          }}
+        >
+          <LoginContainer>
+            <h2>PAGE</h2>
+            <label>¡El libro que buscás, esta acá!</label>
+            <Field
+              name="email"
+              type="email"
+              id="email"
+              as={InputStyled}
+              placeholder="Correo electrónico"
+            ></Field>
+            <ErrorMessage name="email" component={ErrorStyled} />
+            <Field
+              name="password"
+              type="password"
+              id="password"
+              as={InputStyled}
+              placeholder="Contraseña"
+            ></Field>
+            <ErrorMessage name="password" component={ErrorStyled} />
+            <ButtonForm>Inicia sesión</ButtonForm>
+            <h3>¿No tienes una cuenta?</h3>
+            <LinkButton to="/register">Regístrate</LinkButton>
+          </LoginContainer>
+        </Formik>
+      )}
     </>
   );
 };
