c381873a8272f245ea5ce497146fb93fedf0cdc0
ezeabregu <ezeabregu.ea@gmail.com>
2024-07-08 22:53:41 -0300

    cambios user

--- src/pages/user/userStyles.js
+++ src/pages/user/userStyles.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const UserContainerStyled = styled.div`
-  width: 100%;
-  height: auto;
+  width: 100vw;
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
