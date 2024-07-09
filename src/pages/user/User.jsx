4edb2b53b424856d573d1d6f1181177387bfefed
ezeabregu <ezeabregu.ea@gmail.com>
2024-07-09 15:18:06 -0300

    userLogin changes

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,8 +1,25 @@
 import React from "react";
-import { UserContainerStyled } from "./userStyles";
+import { UserContainerStyled, NameUserStyled } from "./userStyles";
+import ButtonDefect from "../../components/ButtonDefect/ButtonDefect";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentUser } from "../../redux/user/userSlice";
 
 const User = () => {
-  return <UserContainerStyled>USER</UserContainerStyled>;
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const dispatch = useDispatch();
+
+  return (
+    <UserContainerStyled>
+      <NameUserStyled>{`Hola ${currentUser?.name}!!`}</NameUserStyled>
+      <ButtonDefect
+        onClick={() => {
+          dispatch(setCurrentUser(null));
+        }}
+      >
+        Cerrar sesión
+      </ButtonDefect>
+    </UserContainerStyled>
+  );
 };
 
 export default User;
--- src/pages/user/userStyles.js
+++ src/pages/user/userStyles.js
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
 export const UserContainerStyled = styled.div`
+  //margin-top: 90px;
   width: 100vw;
   height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+`;
+
+export const NameUserStyled = styled.h2`
+  color: #252525;
 `;
